5aa99851c7a79c8543e6069632a770a7d93652bc
yoyoyoheyheyhey <asokottedotti@icloud.com>
2019-05-07 19:46:58 +0900

    Fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :blog do
+  # ErrorCause: using singular for resources is inaproppreate
+  # FixReason: Fix typo (blog --> blogs)
+  resources :blogs do
     resources :comments, only: [:create, :destroy]
   end
 end
